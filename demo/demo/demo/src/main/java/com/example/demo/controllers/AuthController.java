82a876a942820c6de8c6ff2c97370241964cfe36
Roshini2301 <153311555+Roshini2301@users.noreply.github.com>
2025-07-10 17:56:45 +0530

    Day 6 task,Day 7 mrg and after

---
+++ demo/demo/demo/src/main/java/com/example/demo/controllers/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.RegisterDetails;
+import com.example.demo.models.UserDetailsDto;
+import com.example.demo.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthService authService;
+
+    @PostMapping("/register")
+    public String addNewUser(@RequestBody UserDetailsDto register){
+        return authService.addNewEmployee(register);
+    }
+
+    @PostMapping("/login")
+    public String Login(@RequestBody RegisterDetails login){
+        return authService.authenticate(login);
+    }
+
+}
